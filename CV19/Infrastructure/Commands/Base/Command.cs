05a25ca61be466899be2f03d0def3d0f8dbf3dbe
Andrey MrknownBrain <MrKnowForeverBrain@yandex.ru>
2021-05-05 11:54:05 +0300

    Базовый класс комманды

--- CV19/Infrastructure/Commands/Base/Command.cs
+++ CV19/Infrastructure/Commands/Base/Command.cs
@@ -1,6 +1,18 @@
-﻿namespace CV19.Infrastructure.Commands.Base
+﻿using System;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
 {
-    internal class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
     }
 }
