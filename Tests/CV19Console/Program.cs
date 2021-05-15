8ffe141f1f0004d43dabc73d7bdb9adfbde69fa4
Andrey MrknownBrain <MrKnowForeverBrain@yandex.ru>
2021-05-15 11:02:14 +0300

    Запрос к удаленному серверу для получения сырых данных.

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace CV19Console
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        private static void Main(string[] args)
         {
+            var client = new HttpClient();
+            var responce = client.GetAsync(data_url).Result;
+
+            var csv_str = responce.Content.ReadAsStringAsync().Result;
+
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
